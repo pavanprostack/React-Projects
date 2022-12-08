781f5dc59c5b0bdb50b1c383dd1fcc3d17b9cd75
pavanprostack <pavanprostack@gmail.com>
2022-12-08 15:56:51 +0530

    okk

--- React-Dashboard/dashboard-project/src/components/sidebar/Sidebar.jsx
+++ React-Dashboard/dashboard-project/src/components/sidebar/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import '../sidebar/index.css'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
+import image from '../sidebar/logo.jpeg'
 
 const Sidebar = () => {
 
@@ -17,38 +18,28 @@ const Sidebar = () => {
                 <i className="fa-solid fa-bars"></i>
               </div>
             </header>
-
             <aside className={`sidebar ${show ? 'show' : null}`}>
               <nav className="nav">
-                <div>
-                  <Link to="/" className="nav-logo">
-                    <i className="fas fa-home-alt nav-logo-icon"></i>
-                    <span className="nav-logo-name">Home Page</span>
-                  </Link>
-
+                <div>  
+                <Link to="/" className="nav-logo">
+                  <img src={image} alt="logo" height='70' width='100'/>            
+                  </Link>                
                   <div className="nav-list">
-                    <Link to="/dashboard" className="nav-link active">
-                      <i className="fa-solid fa-gauge-high nav-link-icon"></i>
-                      <span className="nav-link-name">Dashboard</span>
+                     <Link to="/dashboard" className="nav-link active">
+                      <i className="fas fa-home-alt nav-link-icon"></i>
+                      <span className="nav-link-name ">Dashboard</span>
                     </Link>
-                    <Link to="/hotel" className="nav-link">
-                      <i className="fa-solid fa-hotel nav-link-icon"></i>
-                      <span className="nav-link-name">Hotel</span>
+                    <Link to="/approvals" className="nav-link">
+                      <span className="nav-link-name margin">Approvals</span>
                     </Link>
-                    <Link to="/gallery" className="nav-link">
-                      <i className="fa-solid fa-image nav-link-icon"></i>
-                      <span className="nav-link-name">Gallery</span>
+                    <Link to="/contact" className="nav-link">
+                      <span className="nav-link-name margin">Contact Us</span>
                     </Link>
-                    <Link to="/transaction" className="nav-link">
-                      <i className="fa-solid fa-dollar-sign nav-link-icon"></i>
-                      <span className="nav-link-name">Transaction</span>
+                    <Link to="/members" className="nav-link">
+                      <span className="nav-link-name margin">Members</span>
                     </Link>
                   </div>
                 </div>
-                <Link to="/logout" className="nav-link">
-                  <i className="fa-solid fa-sign-out nav-link-icon"></i>
-                  <span className="nav-link-name">Logout</span>
-                </Link>
               </nav>
             </aside>
             <h1>Content</h1>
